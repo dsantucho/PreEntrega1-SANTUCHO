0cfc67a85525fd608206668ae439cc844909f9ad
Daniela Santucho <daniela.santucho@fzsports.com>
2023-04-05 15:28:15 -0300

    cards style arreglo + botton add

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -14,6 +14,7 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Button from '@mui/material/Button';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -34,72 +35,27 @@ const CardItem = ({ data }) => {
   };
 
   return (
-    <Card sx={{ maxWidth: 345, minWidth: 300, minHeight: 375 }}>
+    <Card  className="home-cards-list d-flex flex-column justify-content-between align-self-end">
       <CardMedia
         component="img"
         height="195"
         image={data.image}
         alt={data.title}
       />
-      <CardHeader
-        // action={
-        //   <IconButton aria-label="settings">
-        //     <MoreVertIcon />
-        //   </IconButton>
-        // }
-        title={data.title}
-      />
+      <div>
+        <h3 className="cards-typo m-4">{data.title}</h3>
+      </div>
+      
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {/* {data.Description} */}
-          {data.price}
-        </Typography>
+        <p>{/* {data.Description} */}</p>
+        <p>$ {data.price}</p>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        {/* <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore> */}
-      </CardActions>
-      {/* <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
-          </Typography>
-        </CardContent>
-      </Collapse> */}
+      <div className="d-flex justify-content-center align-item-end ">
+        <button className="button-cards">ADD TO CART</button>
+      </div>
+      
     </Card>
+
   );
 };
 export default CardItem;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,7 +5,7 @@ const CardList = (products) => {
   return (
     <div className='d-flex m-5 flex-wrap justify-content-between'>
     {products.data.map((product) => (
-      <div key={product.id} className = "card d-fex mb-5">
+      <div key={product.id} className = "card d-fex mb-5 justify-content-between">
         <Link to = {`/detail/${product.id}`}>
           <CardItem data={product}/>
         </Link>
